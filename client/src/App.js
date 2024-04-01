231eb7fb6a717283a10cfdab8d6e790f73d7052d
AndersonGuassu <andersonguassu@hotmail.com>
2024-04-01 07:50:46 -0300

    chore: setup nginx and redux toolkit with docker

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,14 @@
 import React from 'react';
-import { Layout } from "antd";
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import AppFooter from "./components/common/AppFooter";
-import AppHeader from "./components/common/AppHeader";
-import HomePage from "./pages/HomePage";
-import PropertyListPage from './pages/PropertyListPage';
 
-const { Content, Header, Footer } = Layout;
-
-function App() {
+const App = () => {
   return (
-    <Router>
-      <Layout className='main-layout'>
-        <Header>
-          <AppHeader />
-        </Header>
-        <Content>
-          <Routes>
-            <Route exact path='/' element={<HomePage/>} />
-            <Route exact path='/properties' element={<PropertyListPage/>} />
-          </Routes>
-        </Content>
-        <Footer>
-            <AppFooter/>
-        </Footer>
-      </Layout>
-    </Router>
+    <div className='App'>
+      <h1>
+        Welcom to the Real Estate
+      </h1>
+    </div>
   );
-}
+};
 
-export default App;
+export default App
